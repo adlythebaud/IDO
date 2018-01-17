66e6b487fcbb2e07de7e3421722ca292639755cc
adlythebaud <adlythebaud@gmail.com>
2018-01-16 22:54:39 -0700

    6 activities created and ready for developing

---
+++ app/src/main/java/com/mycabbages/teamavatar/ido/ChangeItemActivity.java
@@ -0,0 +1,13 @@
+package com.mycabbages.teamavatar.ido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_item);
+    }
+}
