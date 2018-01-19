f4901dd87974bb4cd0852ffbd65ce9c7973ebd0f
adlythebaud <adlythebaud@gmail.com>
2018-01-18 19:12:06 -0700

    ActionBar navigates to home in NewItemActivity

--- app/src/main/java/com/mycabbages/teamavatar/ido/NewItemActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido/NewItemActivity.java
@@ -1,8 +1,11 @@
 package com.mycabbages.teamavatar.ido;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -11,8 +14,46 @@ public class NewItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
-        // set the toolbar as the app bar for the activity.
+        // set the toolbar as the action bar for the activity.
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        // get a reference to the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("New Item Activity");
+
+        // Enable the up button.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
+
+    /*
+        * To use the ActionBar utility methods, call the activity's getSupportActionBar() method.
+        * This method returns a reference to an appcompat ActionBar object. Once you have that
+        * reference, you can call any of the ActionBar methods to adjust the app bar. For example,
+        * to hide the app bar, call ActionBar.hide().
+        * */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_item_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
