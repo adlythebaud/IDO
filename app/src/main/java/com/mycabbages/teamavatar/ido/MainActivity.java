649547da5449574ce5de8cc4b921370b47376c20
Prestonh91 <preston.higgins91@gmail.com>
2018-02-04 15:38:25 -0700

    AddItemActivity working and Firebase structure set up"

--- app/src/main/java/com/mycabbages/teamavatar/ido/MainActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
