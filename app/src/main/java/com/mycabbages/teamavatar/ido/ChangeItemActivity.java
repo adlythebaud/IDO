fdd08bde92e9f6ab226e19d8ae1e4c826a952811
adlythebaud <adlythebaud@gmail.com>
2018-02-07 20:37:00 -0700

    change item activity is complete

--- app/src/main/java/com/mycabbages/teamavatar/ido/ChangeItemActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido/ChangeItemActivity.java
@@ -1,13 +1,144 @@
 package com.mycabbages.teamavatar.ido;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 
-public class ChangeItemActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class ChangeItemActivity extends AppCompatActivity implements DatePickerFragment.DatePickerFragmentListener,
+        TimePickerFragment.TimePickerFragmentListener {
+
+    private final String TAG = "ChangeItemActivity";
+    private ConstraintLayout mConstraintLayout; // ConstraintLayout containing delete button
+    private Switch mSwitch;
+    private EditText datePickerEditText;
+    private Button goalPickerButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_item);
+
+        // get references to everything we need in the UI.
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        mSwitch = (Switch) findViewById(R.id.notificationSettingsSwitch);
+        datePickerEditText = (EditText) findViewById(R.id.datePickerEditText);
+        goalPickerButton = (Button) findViewById(R.id.goalPickerButton);
+
+        // Get reference to ConstraintLayout containing delete button
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.bottomConstraintLayout);
+
+        // set the toolbar as the action bar for the activity.
+        setSupportActionBar(myToolbar);
+
+        // get a reference to the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // set the action bar title
+        actionBar.setTitle("Change Item Activity");
+
+        // Enable the up button.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // set a listener for the switch for when it is checked
+        // hid
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d(TAG, "switch is on");
+                    // do something because the switch is on...
+                    mConstraintLayout.animate().translationY(mConstraintLayout.getHeight() - 60);
+                } else {
+                    Log.d(TAG, "switch is off");
+                    // do something because the switch is off..
+                    mConstraintLayout.animate().translationY(0);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_item_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.done) {
+            Log.d(TAG, "Done button tapped");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * SHOW DATE PICKER DIALOG
+     * */
+    public void showDatePickerDialog(View v) {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(this);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+
+    }
+
+    /**
+     * SHOW TIME PICKER DIALOG
+     * */
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = TimePickerFragment.newInstance(this);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    /**
+     * ON DATE SET
+     * */
+    @Override
+    public void onDateSet(Date date) {
+        Log.d(TAG, date.toString());
+        datePickerEditText.setText(date.toString());
+    }
+
+    /**
+     * ON TIME SET
+     * */
+    @Override
+    public void onTimeSet(Date date) {
+        Log.d(TAG, date.toString());
+    }
+
+    /**
+     * TO ADD ITEM ACTIVITY
+     * Called from UI
+     * */
+    public void toAddItemActivity(View view) {
+        Intent intent = new Intent(this, AddItemActivity.class);
+        startActivity(intent);
     }
 }
