1d317e6ef1f1bc2052cffb6d3416d77225483376
adlythebaud <adlythebaud@gmail.com>
2018-02-24 22:07:04 -0700

    created circular checkbox for day of week checker

--- app/src/main/java/com/mycabbages/teamavatar/ido/NotifSettingsActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido/NotifSettingsActivity.java
@@ -1,14 +1,56 @@
 package com.mycabbages.teamavatar.ido;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NotifSettingsActivity extends AppCompatActivity {
 
+    private final String TAG = "NotifSettingsActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notif_settings);
 
+        // get reference to toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        // set the toolbar as the action bar for the activity.
+        setSupportActionBar(myToolbar);
+
+        // get a reference to the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // set the action bar title
+        actionBar.setTitle("Notification Settings Activity");
+
+        // Display back arrow (up button) in top left and enable it.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notif_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.done) {
+            Log.d(TAG, "Done button tapped");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
