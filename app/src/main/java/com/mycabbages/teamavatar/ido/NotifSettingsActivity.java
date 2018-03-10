8729cf2a80c54672644ab03fab44ff225d3405c5
adlythebaud <adlythebaud@gmail.com>
2018-03-10 13:35:25 -0500

    push notification fails to send. Need to put it in it's own class also.

--- app/src/main/java/com/mycabbages/teamavatar/ido/NotifSettingsActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido/NotifSettingsActivity.java
@@ -1,8 +1,16 @@
 package com.mycabbages.teamavatar.ido;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -41,7 +49,11 @@ public class NotifSettingsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notif_settings);
 
+        // get UI elements
         getUIElements();
+
+        // set up notifications
+        setUpNotificationChannel();
 
         // set the toolbar as the action bar for the activity.
         setSupportActionBar(myToolbar);
@@ -148,15 +160,55 @@ public class NotifSettingsActivity extends AppCompatActivity
     }
 
     /**
+     * Set up notifications from the app.
+     * */
+    public void setUpNotificationChannel() {
+        Log.d(TAG, "Build.version.SDK_INT: " + Build.VERSION.SDK_INT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // get access to Notification Service through a Notification Manager object
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            String channelId = "some_channel_id";
+            CharSequence channelName = "Some Channel";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    /**
      * SEND TEST NOTIFICATION
      * testing out push notification functionality and frequency
      * */
     public void sendTestNotification(View view) {
-//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setSmallIcon(R.mipmap.ic_launcher_round)
-//                .setContentTitle("Test Notification!")
-//                .setContentText("This is a test notification. Does this work?")
-//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this,
+                "some_channel_id")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Test Notification!")
+                .setContentText("This is a test notification. Does this work?")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("This is a test notification. Does this work?"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(001, mBuilder.build());
+
+
     }
+
 
 }
