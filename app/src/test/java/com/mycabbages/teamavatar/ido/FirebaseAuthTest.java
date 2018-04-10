33362d498cb8570ba1349065ce24343f12baee47
adlythebaud <adlythebaud@gmail.com>
2018-04-10 13:41:56 -0600

    writing test for sign up and login with firebase auth

---
+++ app/src/test/java/com/mycabbages/teamavatar/ido/FirebaseAuthTest.java
@@ -0,0 +1,22 @@
+package com.mycabbages.teamavatar.ido;
+
+import android.widget.TextView;
+
+import org.junit.Test;
+
+public class FirebaseAuthTest {
+
+//    @Test
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
+
+    @Test
+    public void sign_Up_With_Firebase() throws Exception {
+        LoginSignUpActivity loginSignUpActivity = new LoginSignUpActivity();
+        TextView showSignInUI = (TextView) loginSignUpActivity.findViewById(R.id.showSignInUI);
+
+
+
+    }
+}
